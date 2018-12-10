42c315fb4e2b47be384f758c7fe48c5ce7968986
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2018-12-11 02:28:06 +0530

    Initial Commit

---
+++ Expense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expense
+//
+//  Created by Khushmeet Singh on 11/12/18.
+//  Copyright © 2018 Khushmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
