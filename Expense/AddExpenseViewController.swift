06ae37ba4477b772556b937b869ced8a9670969a
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2018-12-15 15:21:59 +0530

    added datepicker for addExpenseScreen

--- Expense/AddExpenseViewController.swift
+++ Expense/AddExpenseViewController.swift
@@ -12,35 +12,44 @@ import RealmSwift
 class AddExpenseViewController: UIViewController {
     
     var expenseListScreen = ExpenseTableViewController()
-    let dateFormatter = DateFormatter()
+    var date: Date = Date(timeIntervalSinceReferenceDate: 290000)
     
     @IBOutlet weak var expenseName: UITextField!
     @IBOutlet weak var expenseCategory: UITextField!
-    @IBOutlet weak var expenseDate: UITextField!
     @IBOutlet weak var expenseAmount: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dateFormatter.dateFormat = "dd/MM/yyyy"
+    }
+    
+    @IBAction func getDateFromDatePicker(_ sender: Any) {
+        date = datePicker.date
     }
     
     @IBAction func addExpense(_ sender: Any) {
-        let expense = newExpense(name: expenseName.text!, date: expenseDate.text!, category: expenseCategory.text!, amount: expenseAmount.text!)
+        let expense = newExpense(name: expenseName.text!, date: date, category: expenseCategory.text!, amount: expenseAmount.text!)
         let realm = try! Realm()
         try! realm.write {
+            expense.id = incrementId()
             realm.add(expense)
         }
         expenseListScreen.tableView.reloadData()
         navigationController?.popViewController(animated: true)
     }
     
-    func newExpense(name: String, date: String, category: String, amount: String) -> Expense {
+    func newExpense(name: String, date: Date, category: String, amount: String) -> Expense {        
         let expense = Expense()
         expense.name = name
-        expense.date = dateFormatter.date(from: date)!
+        expense.date = date
         expense.category = category
         expense.amount = Double(amount)!
         return expense
+    }
+    
+    func incrementId() -> Int {
+        let realm = try! Realm()
+        return (realm.objects(Expense.self).max(ofProperty: "id") as Int? ?? 0) + 1
     }
     
 }
