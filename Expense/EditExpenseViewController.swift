e35861c0fb2472d45cff35dd13275aceab12bc2f
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2018-12-22 03:13:16 +0530

    added picker view to edit expense screen

--- Expense/EditExpenseViewController.swift
+++ Expense/EditExpenseViewController.swift
@@ -9,21 +9,24 @@
 import UIKit
 import RealmSwift
 
-class EditExpenseViewController: UIViewController {
+class EditExpenseViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     var expenseListScreen = ExpenseTableViewController()
     var selectedExpense = Expense()
     
     @IBOutlet weak var expenseName: UITextField!
-    @IBOutlet weak var expenseCategory: UITextField!
+    @IBOutlet weak var expenseCategoryPicker: UIPickerView!
     @IBOutlet weak var expenseAmount: UITextField!
     @IBOutlet weak var expenseDate: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+        self.expenseCategoryPicker.dataSource = self
+        self.expenseCategoryPicker.delegate = self
+        
         expenseName.text = selectedExpense.name
-        expenseCategory.text = selectedExpense.category
+        expenseCategoryPicker.selectRow(expenseCategories.index(where: {$0.categoryName == selectedExpense.category})!, inComponent: 0, animated: true)
         expenseDate.setDate(selectedExpense.date, animated: true)
         expenseAmount.text = String(selectedExpense.amount)
     }
@@ -33,12 +36,24 @@ class EditExpenseViewController: UIViewController {
         if let expense = realm.object(ofType: Expense.self, forPrimaryKey: selectedExpense.id) {
             try! realm.write {
                 expense.name = expenseName.text!
-                expense.category = expenseCategory.text!
+                expense.category = expenseCategories[expenseCategoryPicker.selectedRow(inComponent: 0)].categoryName
                 expense.date = expenseDate.date
                 expense.amount = Double(expenseAmount.text!)!
             }
             expenseListScreen.tableView.reloadData()
             navigationController?.popViewController(animated: true)
         }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return expenseCategories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return expenseCategories[row].categoryName
     }
 }
