675d8045285b625a5119e00885f978bbaf882b79
Khushmeet Singh <khushmeetsingh1996@gmail.com>
2018-12-28 22:15:10 +0530

    added a basic analysis page using categories

--- Expense/ExpenseAnalysisViewController.swift
+++ Expense/ExpenseAnalysisViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ExpenseAnalysisViewController: UIViewController {
+class ExpenseAnalysisViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var expenseCategoryPicker: UIPickerView!
+    @IBOutlet weak var categoryExpenditure: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.expenseCategoryPicker.dataSource = self
+        self.expenseCategoryPicker.delegate = self
+        expenseCategoryPicker.selectRow(3, inComponent: 0, animated: true)
     }
+    
+    @IBAction func calculateExpenditure(_ sender: Any) {
+        var expenditure = 0.0
+        let selectedCategory = expenseCategories[expenseCategoryPicker.selectedRow(inComponent: 0)].categoryName
+        let expensesOfCategory = try! Realm().objects(Expense.self).filter("category = %@", selectedCategory)
+        for item in expensesOfCategory {
+            if item.txnType == 1 {
+                expenditure += item.amount
+            } else {
+                expenditure -= item.amount
+            }
+        }
+        formatExpenditure(prefix: "₹", for: expenditure)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return expenseCategories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return expenseCategories[row].categoryName
+    }
+    
+    func formatExpenditure(prefix:String, for expenditure:Double) {
+        categoryExpenditure.text = prefix + String(format: "%2.2f", expenditure)
+    }
+    
 
 }
